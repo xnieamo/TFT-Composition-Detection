409e9ba8e0bb2e9dbbcfd5f236a06c791a1d7885
xiaomaoding1@gmail.com <xiaomaoding1@gmail.com>
2023-06-13 22:36:03 -0400

    check for update before downloading

--- JSONDownloader/DownloadData.py
+++ JSONDownloader/DownloadData.py
@@ -25,7 +25,7 @@ def get_current_version():
         return data["version"]
 
 def update_data():
-    # if has_update():
+    if has_update():
         url = "https://raw.communitydragon.org/latest/cdragon/tft/en_us.json"
         response = requests.get(url)
         if response.status_code == 200:
@@ -35,8 +35,6 @@ def update_data():
             abs_file_path = os.path.join(script_dir, rel_path)
             with open(abs_file_path, "w") as f:
                 json.dump(data, f, indent=4)
-        return 0
-    # else:
-        return 0
+
 
 print(update_data())
