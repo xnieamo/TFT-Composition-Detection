b3fe249f628702723cf7befd9d4e94b482615903
xiaomaoding1@gmail.com <xiaomaoding1@gmail.com>
2023-06-13 22:35:26 -0400

    init and download data

---
+++ JSONDownloader/DownloadData.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import os
+
+
+def has_update():
+
+    url = "https://raw.communitydragon.org/latest/content-metadata.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        versions = json.loads(response.text)
+        latest_version = versions["version"]
+        return latest_version != get_current_version()
+    else:
+        return True
+
+def get_current_version():
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    rel_path = "../Data/content-metadata.json"
+    abs_file_path = os.path.join(script_dir, rel_path)
+
+    with open(abs_file_path, "r") as f:
+        data = json.load(f)
+        return data["version"]
+
+def update_data():
+    # if has_update():
+        url = "https://raw.communitydragon.org/latest/cdragon/tft/en_us.json"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            rel_path = "../Data/en_us.json"
+            abs_file_path = os.path.join(script_dir, rel_path)
+            with open(abs_file_path, "w") as f:
+                json.dump(data, f, indent=4)
+        return 0
+    # else:
+        return 0
+
+print(update_data())
---
+++ readme.md
@@ -0,0 +1,6 @@
+# TODO LIST
+- [ ] Roll calculator
+- [ ] JSON importer
+    - [ ] Automatic JSON downloader
+- [ ] Champ DPS calculator
+    - [ ] DPS time line 
